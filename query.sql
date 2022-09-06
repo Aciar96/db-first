633573957e35cfe83b23ed44caa016be8215261a
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-09-06 15:38:37 +0200

    query con select n3

--- query.sql
+++ query.sql
@@ -7,7 +7,7 @@ SELECT `name`,`date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%
 SELECT `cfu` FROM `courses` WHERE `cfu` > '10';
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT `id`,`date_of_birth` FROM `students` WHERE `date_of_birth`< '1992-09-06' ORDER BY `date_of_birth` ASC;
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 
