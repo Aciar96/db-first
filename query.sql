ded7bb9abdeb2bdf7254a40ba814653afd464ce7
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-09-06 15:10:01 +0200

    query con select n1

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 -- ---------------------------------------  QUERY CON SELECT
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-
+SELECT `name`,`date_of_birth` FROM `students` WHERE `date_of_birth` LIKE '1990-%-%' ORDER BY `date_of_birth` ASC;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 
